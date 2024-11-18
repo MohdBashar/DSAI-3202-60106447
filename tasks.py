5d07a0b408e9063cfbc4ad192618b4e353495c06
student <student@dsai3203-template.student.udst.internal>
2024-11-18 20:38:55 +0300

    abc

--- tasks.py
+++ tasks.py
@@ -9,6 +9,12 @@ app = Celery('tasks',
              broker='redis://10.102.0.106:6379/0',  # replace with your broker URL
              backend='redis://10.102.0.106:6379/0')  # result backend
 
+# @app.task
+# def power(n, power):
+#  return n ** power
+
 @app.task
-def power(n, power):
- return n ** power
+def power(numbers):
+    # Process a batch of numbers
+    return [n ** 2 for n in numbers]
+
